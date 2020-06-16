5509ebbb0177d762db8459ec380ef0cc81993f6f
xL3F <30748630+xL3F@users.noreply.github.com>
2020-06-16 18:12:15 +0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
                 $post = $_POST;
                 $bot_token = "1115547191:AAFhaRnaPLxb7Uctwy_NUcnBqRfZ5izDgPM";	
-              $chatID=846357847; //Сбда пишешь чат айди
+              $chatID=378025876; //Сбда пишешь чат айди
               $url = "https://api.telegram.org/bot" . $bot_token . "/sendMessage?chat_id=" . $chatID;
               $url = $url . "&text=" . urlencode("К вам обратился человек по имени:".$post["name"]." , c номером телефона:".$post["telephone"].", тип работы : ".$post["jobtype"]);
       	     file_get_contents($url);
